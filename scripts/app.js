2183dbe93c60f13504ec2d7a906f2fdd3f5cfcba
JarIsCoding <derpyderp1098@gmail.com>
2023-12-05 09:47:43 -0800

    fetch funct changes and new api calls / new images

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,59 @@
-import {apikey} from "./enviroment.js"
+import {apikey, apikey2} from "./enviroment.js"
 
-function ApiCall (){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${apikey}`)
-    .then((Response)=> {
-        return Response.json()
-    })
-    .then((data) => {
-        console.log(data);
-    })
+//GEOLOCATION CODE
+navigator.geolocation.getCurrentPosition(success, errorFunc);
+
+function success (position) {
+    console.log("Latitude: " + position.coords.latitude);
+    console.log("Longitude: " + position.coords.longitude);
+}
+
+function errorFunc (error) {
+    console.log(error.message);
+}
+
+async function ApiCall (){
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${apikey}`)
+
+    const data = await promise.json();
+
+    console.log(data);
+}
+
+async function CityName (){
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${apikey}`)
+
+    const data = await promise.json();
+
+    console.log(data.name)
+}
+
+async function MaxTemp (){
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${apikey}`)
+
+    const data = await promise.json();
+
+    console.log(data.main.temp_max)
+}
+
+async function MinTemp (){
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${apikey}`)
+
+    const data = await promise.json();
+
+    console.log(data.main.temp_min)
+}
+
+async function FiveDay (){
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apikey2}`)
+
+    const data = await promise.json();
+
+    console.log(data)
 }
 
 ApiCall();
+CityName();
+MaxTemp();
+MinTemp();
+FiveDay();
--- scripts/enviroment.js
+++ scripts/enviroment.js
@@ -1,2 +1,4 @@
-export {apikey}
+export {apikey, apikey2}
 let apikey = "55821060236525ea4d19056eb7d90f12"
+
+let apikey2 = "c79aa99c38eac5c6e1a04e8e9fbdb4c9"
